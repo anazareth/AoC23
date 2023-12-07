72c86529aecca24e2fe0a297f533f8841c841971
Alex Nazareth <anazareth@ryerson.ca>
2023-12-07 15:23:58 -0500

    day 01 complete

---
+++ day01/day01.py
@@ -0,0 +1,39 @@
+'''
+Advent of Code 2023 Day 01
+https://adventofcode.com/2022/day/1
+AUTHOR: Alex Nazareth
+DATE:   November 2023
+Short description:
+ - 
+'''
+
+def part1(data):
+    result = 0
+    for row in data:
+        ints = [r for r in row if r.isnumeric()]
+        if len(ints) > 0:
+            if len(ints) == 1:
+                result += int(ints[0] + ints[0])
+            else:
+                result += int(''.join([ints[i] for i in [0,-1]]))
+    return result
+
+def part2(data):
+    numname_to_num = {'one': 'o1e', 'two': 't2o', 'three': 't3e', 'four': 'f4r', 'five': 'f5e',
+                       'six': 's6x', 'seven': 's7n', 'eight': 'e8t', 'nine': 'n9e'}
+    data2 = []
+    for row in data:
+        for numname, num in numname_to_num.items():
+            row = row.replace(numname, num)
+        data2.append(row)
+    return part1(data2)
+
+
+if __name__ == "__main__":
+    filename = "input.txt"
+    with open(filename) as f:
+        rawdata = f.read().splitlines()
+
+    print("Advent of Code 2023 Day 01")
+    print("PART ONE: " + str(part1(rawdata)))
+    print("PART TWO: " + str(part2(rawdata)))
